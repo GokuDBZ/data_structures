ad435d0b593f89aadec4f3327052185de8aaca42
Athioush Ranjan <athioushranjan@gmail.com>
2020-07-11 12:31:35 +0530

    two sum problem

---
+++ go/programs/two_sum.go
@@ -0,0 +1,13 @@
+package programs
+
+func twoSum(nums []int, target int) []int{
+	size := len(nums)-1
+	for i, _:= range nums{
+		for j:=i+1; j<= size; j++ {
+			if target == nums[i] + nums[j]{
+				return []int{i,j}
+			}
+		}
+	}
+	return []int{-1,-1}
+}
