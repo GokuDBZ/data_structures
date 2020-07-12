d0d2aeb630800e4551f45fe0de2aeb2049e5ef27
Athioush Ranjan <athioushranjan@gmail.com>
2020-07-13 02:04:26 +0530

    Bubble Sort

---
+++ go/programs/bubble_sort.go
@@ -0,0 +1,51 @@
+package programs
+
+import "fmt"
+
+func BubbleSort(array []int) []int {
+	size := len(array)-1
+	var iteration, swap int
+	for _= range array{
+		swap = 0
+		for index:= 1; index <= size-iteration; index++{
+			if array[index-1] > array[index]{
+				swap++
+				temp := array[index]
+				array[index] = array[index-1]
+				array[index-1] = temp
+			}
+			if swap == 0 && index== size -iteration{
+				return array
+			}
+			fmt.Println("--",array)
+		}
+		iteration++
+		fmt.Println("-",array)
+	}
+
+	return array
+
+	// we can also apply the below code for the bubble sort
+	//check the code after uncommenting it
+	/*
+		size := len(array)-1
+		var iteration int
+		isSorted:= false
+		for !isSorted{
+			isSorted = true
+			for index:= 1; index <= size-iteration; index++{
+				if array[index-1] > array[index]{
+					isSorted = false
+					temp := array[index]
+					array[index] = array[index-1]
+					array[index-1] = temp
+				}
+				fmt.Println("--",array)
+			}
+			iteration++
+			fmt.Println("-",array)
+		}
+
+		return array
+	*/
+}
