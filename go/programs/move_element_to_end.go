636d836d1d8e8c3fce0a417b755e2cb44ecd39dc
Athioush Ranjan <athioushranjan@gmail.com>
2020-07-18 20:28:50 +0530

    Move element to end

--- go/programs/move_element_to_end.go
+++ go/programs/move_element_to_end.go
@@ -2,30 +2,25 @@ package programs
 
 /*
 	Input samples{{-2,-2,2,2,-2,4,1,2,0,5,-2,4,2,0,1,5,2,2,-2,-2},{-2,2,2,2,2,4,1,2,0,5,2,4,2,0,1,5,2,2,2,2},
-		{4,1,2,0,5,2,4,2,0,1,5,2,2}}
+		{4,1,2,0,5,2,4,2,0,1,5,2,2},{2,1,2,2,2,3,4,2}}
 	Time complexity will be O(n)
 	Space complexity will be O(1)
  */
 
 func MoveElement(inputArray []int, toMove int) []int{
+	startPointer := 0
 	endPointer := len(inputArray)- 1
-	for startPointer, value := range inputArray{
-		if startPointer > endPointer{
-			break
+	for startPointer < endPointer{
+		for startPointer < endPointer && inputArray[endPointer] == toMove {
+				endPointer--
 		}
-		if value == toMove {
-			for {
-				if inputArray[endPointer] == toMove {
-					endPointer--
-				}else{
-					break
-				}
-			}
+		if inputArray[startPointer] == toMove {
 			// moving the require number to the last of array
 			inputArray[startPointer] = inputArray[endPointer]
 			inputArray[endPointer] = toMove
 			endPointer--
 		}
+		startPointer++
 	}
 	return inputArray
 }
