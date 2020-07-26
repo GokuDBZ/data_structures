d9400cfaf249ff00a264d732dbc0da8d37e6cc5e
Suraj Yadav <suraj.yadav@modaoperandi.com>
2020-07-26 22:46:07 +0530

    trying terraform

--- s3.tf
+++ s3.tf
@@ -1,6 +1,8 @@
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
+  access_key = "AKIA4JRHLZCVM636PP5P"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "b" {
