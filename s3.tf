75c2a5b21e0f31c5c727c12c767b279238c1ba05
Suraj Yadav <suraj.yadav@modaoperandi.com>
2020-07-28 22:57:26 +0530

    terraform state

--- s3.tf
+++ s3.tf
@@ -1,10 +1,21 @@
 
 provider "aws" {
   region     = "us-west-2"
+  access_key = "AKIA4JRHLZCVM636PP5P"
+  secret_key = "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"
+
+}
+
+terraform {
+  backend "s3" {
+    bucket = "demo.zubbi"
+    key    = "app/dev/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "demo.suraj"
+  bucket = "demo.srj"
   acl    = "private"
 
   tags = {
