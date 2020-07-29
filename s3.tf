5026b4c87d7a67aa6d6dcd37c28d1315ee98b84e
Suraj Yadav <suraj.yadav@modaoperandi.com>
2020-07-29 09:39:43 +0530

    trying backend again

--- s3.tf
+++ s3.tf
@@ -3,13 +3,13 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "demo.zubbi"
-    key    = "app/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "demo.zubbi"
+#     key    = "app/dev/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 resource "aws_s3_bucket" "demo_ishu" {
   bucket = "demo.ishu"
