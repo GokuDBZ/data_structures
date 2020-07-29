1ec231722be4407d3de9c7a09e594af54a8bbe2d
athioushranjan <30904151+athioushranjan@users.noreply.github.com>
2020-07-29 11:10:45 +0530

    Smallest Difference (#27)
    * Smallest Difference
    
    * Algoexpert programs

---
+++ go/Algoexpert.io/programs/insertion_sort.go
@@ -0,0 +1,26 @@
+package programs
+
+func InsertionSort(inputArray []int) []int {
+	for index, _:= range inputArray {
+		j := index
+		for j >0 {
+			if inputArray[j] < inputArray[j-1]{
+				// calling swap method or we can write the same code for swap
+				swap(j, j-1, inputArray)
+			}else {
+				break
+			}
+			// decrementing value
+			j-=1
+// 			fmt.Println("--",inputArray)
+		}
+// 		fmt.Println("-",inputArray)
+	}
+	return inputArray
+}
+// for swapping value
+func swap(j , i int, array []int){
+	temp := array[j]
+	array[j] = array[i]
+	array[i] = temp
+}
---
+++ go/Algoexpert.io/programs/two_sum_with_sort.go
@@ -0,0 +1,34 @@
+package programs
+import(
+	"fmt"
+	"sort"
+)
+
+/*
+	we have done sorting and then try to find out the
+	target sum value and return the values whose sum give the target value
+ */
+func twoSumWithSort(nums []int, target int) []int {
+	sort.Ints(nums) //sorting the array
+	left := 0
+	right := len(nums)-1
+	fmt.Printf("initial value left: %v right: %v\n", left, right)
+	fmt.Println("slice",nums)
+	for _, _ = range nums {
+		if target > nums[left] + nums[right]{
+			left++
+			fmt.Println("left: ",left)
+		}else if target < nums[left] + nums[right]{
+			right--
+			fmt.Println("right: ",right)
+		}
+		if target == nums[left] + nums[right]{
+			return []int{left,right}
+		}
+
+		if left == right{
+		break
+		}
+	}
+	return []int{-1,-1}
+}
