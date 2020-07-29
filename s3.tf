783b2ccd044ee799e53ed4ad17ffd9e0392273b9
Suraj Yadav <suraj.yadav@modaoperandi.com>
2020-07-29 10:16:02 +0530

    trying backend again

--- s3.tf
+++ s3.tf
@@ -7,16 +7,16 @@ terraform {
   backend "s3" {
     bucket = "demo.zubbi"
     key    = "app/dev/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
-resource "aws_s3_bucket" "demo_ishu" {
-  bucket = "demo.ishu"
+resource "aws_s3_bucket" "demo_athioush" {
+  bucket = "demo.athioush"
   acl    = "private"
   region = "us-east-1"
 
   tags = {
-    Name        = "Demo SRJ"
+    Name        = "athioush"
   }
 }
